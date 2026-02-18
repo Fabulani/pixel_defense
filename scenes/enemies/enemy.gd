7b0e186b71b2f361f88120b9adc3d00103474bc3
Fabulani <16107182+Fabulani@users.noreply.github.com>
2026-02-18 08:18:52 +0200

    Add enemy health

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -1,22 +1,23 @@
-extends Area2D
+class_name Enemy extends Area2D
 
-var path_follow: PathFollow2D
+var _path_follow: PathFollow2D
+var _health = 3
 
 func setup(new_path_follow: PathFollow2D):
-	path_follow = new_path_follow
+	_path_follow = new_path_follow
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("enemies")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	path_follow.progress += 100*delta
-	if path_follow.progress_ratio >= 0.99:
+	_path_follow.progress += 100*delta
+	if _path_follow.progress_ratio >= 0.99:
 		print("damage")
 		queue_free()
 
 
 func _on_area_entered(bullet: Area2D):
+	_health -= 1
+	if _health <= 0:
+		self.queue_free()
 	bullet.queue_free()
