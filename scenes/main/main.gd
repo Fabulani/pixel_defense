95e955bd270670522e17099e72561b97ddfd6132
Fabulani <16107182+Fabulani@users.noreply.github.com>
2026-02-20 15:48:08 +0200

    Refactor tower placement logic into BuildingManager class

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,22 +1,11 @@
 class_name Main extends Node2D
 
+@export var building_manager : BuildingManager = null
 @export var tower_packed_scene : PackedScene = null
-@onready var tile_map_layer: TileMapLayer = $Level/BG/TileMapLayer
-@onready var highlight_tile: HighlightTile = $HighlightTile
-
-const IS_BUILDABLE : String = "buildable"
-const CELL_SIZE : int = 16
+@onready var tile_map_layer: TileMapLayer = $Level/TileMapLayer
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		var cell_position : Vector2i = tile_map_layer.local_to_map(tile_map_layer.get_local_mouse_position())
-		var cell_data = tile_map_layer.get_cell_tile_data(cell_position).get_custom_data(IS_BUILDABLE)
 
-		if cell_data == true:
-			place_tower(cell_position)
-		
-		
-func place_tower(cell_position : Vector2i) -> void:
-	var new_tower : Node2D = tower_packed_scene.instantiate()
-	new_tower.position = cell_position * CELL_SIZE + Vector2i(CELL_SIZE/2, CELL_SIZE/2)
-	add_child(new_tower)
+		building_manager.place_tower(cell_position, tower_packed_scene)
---
+++ scenes/managers/building_manager.gd
@@ -0,0 +1,26 @@
+class_name BuildingManager extends Node
+
+@export var level_layer: Node2D = null
+
+const IS_BUILDABLE : String = "buildable"
+const CELL_SIZE : int = 16
+const TOWER_GROUP : String = "tower_group"
+
+var used_tiles : Array[Vector2i] = []
+
+func place_tower(cell_position : Vector2i, tower_packed_scene : PackedScene) -> void:
+	if not check_valid_tower_placement(cell_position):
+		return
+		
+	var new_tower : Node2D = tower_packed_scene.instantiate()
+	new_tower.position = cell_position * CELL_SIZE + Vector2i(CELL_SIZE/2, CELL_SIZE/2)
+	new_tower.add_to_group(TOWER_GROUP) 	
+	add_child(new_tower)
+
+func check_valid_tower_placement(cell_position : Vector2i) -> bool:
+	if used_tiles.has(cell_position):
+		return false
+		
+	var tile_map_layer = level_layer.get_node("TileMapLayer")
+	var is_buildable_tile = tile_map_layer.get_cell_tile_data(cell_position).get_custom_data(IS_BUILDABLE)
+	return is_buildable_tile
