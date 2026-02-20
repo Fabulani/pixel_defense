ed49e7870a9d160fe130a1e14882acec4df0a82b
Fabulani <16107182+Fabulani@users.noreply.github.com>
2026-02-20 12:32:41 +0200

    Add simple tower placement

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,11 +1,18 @@
 class_name Main extends Node2D
 
-
+@export var tower_packed_scene : PackedScene = null
 @onready var highlight_tile: HighlightTile = $HighlightTile
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
-		print("click")
 		# TODO: tower building. Come back after tileset has custom data eg 'buildable'
-	
+		var test_pos := Vector2i(3, 3) 
+		place_tower(test_pos)
+		
+		
+func place_tower(cell_position : Vector2i) -> void:
+	var new_tower : Node2D = tower_packed_scene.instantiate()
+	new_tower.position = cell_position * 16 + Vector2i(8, 8)
+	add_child(new_tower)
+	# TODO: tower is drawing on top of highlight
