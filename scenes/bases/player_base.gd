d37a98444ff54bcf91bffcd0ddcf5e09943bde42
Fabulani <16107182+Fabulani@users.noreply.github.com>
2026-02-23 13:15:59 +0200

    Fix player base collision with new EnemyEntity

--- scenes/bases/player_base.gd
+++ scenes/bases/player_base.gd
@@ -2,9 +2,10 @@ class_name PlayerBase extends Area2D
 
 var _health = 1
 
-func _on_area_entered(enemy: Enemy) -> void:
-	_health -= 1
-	enemy.queue_free()
-	if _health <= 0:
-		queue_free()
-		print("Game Over")
+func _on_body_entered(body: EnemyEntity) -> void:
+	if body is EnemyEntity:
+		_health -= 1
+		body.queue_free()
+		if _health <= 0:
+			queue_free()
+			print("Game Over")
