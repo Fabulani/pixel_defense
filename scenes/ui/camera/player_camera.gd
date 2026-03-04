3040969c7496f9d24ef11019a851cc298d1be9ee
Fabulani <16107182+Fabulani@users.noreply.github.com>
2026-03-04 23:18:55 +0200

    Improve zooming in with buttons

--- scenes/ui/camera/player_camera.gd
+++ scenes/ui/camera/player_camera.gd
@@ -1,9 +1,5 @@
 class_name PanningCamera extends Camera2D
 
-# TODO: finish fixing up the zooming buttons
-# ZOOM_BTN_INCREMENT should be higher so the buttons feel good to use
-# maybe add the increment as a paramenter to zoom_in/_out?
-
 const MIN_ZOOM: float = 1.0
 const MAX_ZOOM: float = 5.0
 const ZOOM_INCREMENT: float = 0.25
@@ -15,6 +11,7 @@ var _target_zoom: float = 2.0
 
 func _unhandled_input(event: InputEvent) -> void:
 	_handle_mouse_input(event)
+	_handle_touch_input(event)
 
 func _handle_mouse_input(event: InputEvent) -> void:
 	# Panning
@@ -26,13 +23,16 @@ func _handle_mouse_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-				zoom_in()
+				zoom_in(ZOOM_INCREMENT)
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-				zoom_out()
+				zoom_out(ZOOM_INCREMENT)
 
 func _handle_touch_input(event: InputEvent) -> void:
+	# Panning
 	if event is InputEventScreenDrag:
 		position -= event.relative / zoom
+		
+	# TODO: zooming with pinching, and double-tab + drag up/down
 
 func _handle_keyboard_input(_delta: float) -> void:
 	# Panning with WASD or arrow keys
@@ -48,12 +48,12 @@ func _handle_keyboard_input(_delta: float) -> void:
 	if direction != Vector2.ZERO:
 		position += direction.normalized() * KEYBOARD_PAN_SPEED * _delta
 
-func zoom_in() -> void:
-	_target_zoom = min(_target_zoom + ZOOM_INCREMENT, MAX_ZOOM)
+func zoom_in(increment: float) -> void:
+	_target_zoom = min(_target_zoom + increment, MAX_ZOOM)
 	set_physics_process(true)
 
-func zoom_out() -> void:
-	_target_zoom = max(_target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
+func zoom_out(decrement: float) -> void:
+	_target_zoom = max(_target_zoom - decrement, MIN_ZOOM)
 	set_physics_process(true)
 	
 func _physics_process(_delta: float) -> void:
@@ -63,8 +63,10 @@ func _physics_process(_delta: float) -> void:
 func _process(_delta: float) -> void:
 	_handle_keyboard_input(_delta)
 
+# TODO: move this to UI
 func _on_hud_zoom_in_btn_pressed() -> void:
-	zoom_in() 
+	zoom_in(ZOOM_BTN_INCREMENT) 
 
+# TODO: move this to UI
 func _on_hud_zoom_out_btn_pressed() -> void:
-	zoom_out()
+	zoom_out(ZOOM_BTN_INCREMENT)
