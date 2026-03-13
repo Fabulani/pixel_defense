0261f4a695bda03cc5a3c0e84ffe69b312f4a4df
Fabulani <16107182+Fabulani@users.noreply.github.com>
2026-03-13 06:04:35 +0200

    Minor Level refactor

--- levels/level.gd
+++ levels/level.gd
@@ -1,38 +1,38 @@
 class_name Level extends Node2D
 
+@export var player_base: PlayerBase
 @export var enemy_spawner: EnemySpawner
 @export var wave_manager: WaveManager
 @export var projectile_manager: ProjectileManager
-
-@onready var target_pos: Vector2 = $PlayerBase.global_position
-@onready var pathfinding_preview: Line2D = $PathfindingPreview
+@export var pathfinding_preview: Line2D
 
 func _ready() -> void:
 	# Starting currency
 	CurrencyManager.coins = 5
 	
 	PathfindingManager.setup($TileMapLayer)
-	enemy_spawner.target_pos = target_pos
+	enemy_spawner.target_pos = player_base.global_position
 	RenderingServer.set_default_clear_color('dff6f5')
 	
 	_update_pathfinding_preview()
 
 func _on_building_manager_new_tower_built(tower: Tower, cell_position: Vector2i) -> void:
+	# TODO: move this to PathfindingManager
 	PathfindingManager.set_cell_solid(cell_position, true)
+	# TODO: research if there is a better way to handle groups other than get_tree()...
 	for enemy in get_tree().get_nodes_in_group("enemies"):
 		enemy.pathfinding.recalculate_path()
 	
+	# TODO: put this in the building manager. It builds towers, then should also set them up
+	# TODO2: make projectile manager an auload
 	tower.shoot.connect(projectile_manager.create_bullet)
 	_update_pathfinding_preview()
 
 func _update_pathfinding_preview() -> void:
-	var spawn_pos := enemy_spawner.global_position
-	@warning_ignore("integer_division")
-	var path := PathfindingManager.get_valid_path(
-		Vector2i(spawn_pos) / 16, Vector2i(target_pos) / 16
-	)
+	""" Calculate a path from the Spawner to the Player Base. """
+	var path := PathfindingManager.get_valid_path_world(enemy_spawner.global_position, player_base.global_position)
 	pathfinding_preview.clear_points()
-	for point in path:
+	for point in path: 
 		pathfinding_preview.add_point(Vector2(point))
 
 func is_cell_buildable(cell_position: Vector2i) -> bool:
